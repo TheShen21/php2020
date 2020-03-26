e8afd9084724781e76596b16b6360e4f9a2cc17d
theshen <bshen@roc-dev.com>
2020-03-26 15:31:23 +0100

    hoi2

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
-<?php
-echo "hello";
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="en">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>inhoudsopgave</title>
+</head>
+<body>
+<a href="test.php">test</a>
+</body>
+</html>
